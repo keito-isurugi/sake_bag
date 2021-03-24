93e6f08589ad8ea5aca8b06e611fe8b8ff27364b
KeitoIsurugi <ja224kg@gmail.com>
2021-03-25 02:22:31 +0900

    home root

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,14 @@ class StaticPagesController < ApplicationController
 
   def about
   end
-  
+
+  def contact
+  end
+
+  def terms_of_service
+  end
+
+  def privacy_policy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   
+  get 'users/new'
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/terms_of_service', to: 'static_pages#terms_of_service'
+  get '/privacy_policy', to: 'static_pages#privacy_policy'
+  get '/signup', to: 'users#new'
 end
